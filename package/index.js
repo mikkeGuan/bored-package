6762cf70b9aada9b1535c17c89d7250fed8b42c8
Ruichao Guan <mikkeguan@gmail.com>
2023-11-09 21:14:51 +0200

    axios fix

--- package/index.js
+++ package/index.js
@@ -1,49 +1,42 @@
-// boredapi-package.js
 const axios = require('axios');
 
 const BoredAPI = {
   // Fetch a random activity
-  getRandomActivity: () => {
-    return axios
-      .get('https://www.boredapi.com/api/activity')
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        throw new Error('Failed to fetch random activity');
-      });
+  getRandomActivity: async () => {
+    try {
+      const response = await axios.get('https://www.boredapi.com/api/activity');
+      return response.data;
+    } catch (error) {
+      throw new Error('Failed to fetch random activity');
+    }
   },
 
   // Fetch an activity by a specific type
-  getActivityByType: (type) => {
-    return axios
-      .get(`https://www.boredapi.com/api/activity?type=${type}`)
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        throw new Error('Failed to fetch activity by type');
-      });
+  getActivityByType: async (type) => {
+    try {
+      const response = await axios.get(`https://www.boredapi.com/api/activity?type=${type}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Failed to fetch activity by type');
+    }
   },
-  getActivityByParticipants: (participants) => {
-    return axios
-      .get(`https://www.boredapi.com/api/activity?participants=${participants}`) //Participants ranges from 1-5
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        throw new Error('Failed to fetch activity by participants');
-      });
+
+  getActivityByParticipants: async (participants) => {
+    try {
+      const response = await axios.get(`https://www.boredapi.com/api/activity?participants=${participants}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Failed to fetch activity by participants');
+    }
   },
-  getActivityByPrice: (price) => {
-    return axios
-      .get(`https://www.boredapi.com/api/activity?price=${price}`) // 0 is free and pricing goes up to 0.6
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        throw new Error('Failed to fetch activity by price');
-      });
+
+  getActivityByPrice: async (price) => {
+    try {
+      const response = await axios.get(`https://www.boredapi.com/api/activity?price=${price}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Failed to fetch activity by price');
+    }
   },
 };
 
